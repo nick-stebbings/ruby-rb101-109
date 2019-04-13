47b489cc63f2691eb7e7093f0cf352acaa072e61
Nick Stebb <nickofstebbs@gmail.com>
2019-04-12 21:54:48 -0700

    small problems update

---
+++ small_problems/Easy_4/ex6.rb
@@ -0,0 +1,21 @@
+=begin
+def running_total(array)
+  array.each_with_object([]) do |element, memo|
+    memo.length.zero? ? memo << element : memo << element + memo.last
+  end
+end
+=end
+require 'pry'
+def running_total(array)
+  results = []
+  array.inject(0) do |sum,el|
+    binding.pry
+    sum + el
+    results << sum
+  end
+  p results
+end
+p running_total([2, 5, 13]) == [2, 7, 20]
+p running_total([14, 11, 7, 15, 20]) == [14, 25, 32, 47, 67]
+p running_total([3]) == [3]
+p running_total([]) == []
